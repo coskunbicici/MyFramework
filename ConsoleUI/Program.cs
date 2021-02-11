7664c401f34b3d362c081a1d62e18d7d683fc745
coskunbicici <coskun.bicici@gmail.com>
2021-02-11 13:20:15 +0300

    product name and category name are listed

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -30,10 +30,24 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                Console.WriteLine(" ---------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("| " + "{0,-50}" + " | " + "{1,-50}" + " | ", "Ürün Adı", "Kategori Adı");
+                Console.WriteLine(" ---------------------------------------------------------------------------------------------------------");
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("| " + "{0,-50}" + " | " + "{1,-50}" + " | ", product.ProductName, product.CategoryName);
+                }
+                Console.WriteLine(" ---------------------------------------------------------------------------------------------------------");
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
         }
     }
 }
